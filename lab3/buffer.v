8d12e2dfdaaaad9dfe32c0c234a19bed6c3e8a02
Wenbing Zhang <zhangw24@uw.edu>
2016-11-15 13:26:16 -0800

    buffer change

--- lab3/buffer.v
+++ lab3/buffer.v
@@ -1,6 +1,6 @@
 module buffer(data_count, scanning, flush, transfer, clk ,rst);
 
-	output reg [6:0] data_count = 7'd0;
+	output reg [7:0] data_count = 8'd0;
 	input rst, clk, scanning, flush, transfer;
 	reg [24:0] tBase = 25'b0;
 	reg [1:0] semaphore = 2'b00;
@@ -14,20 +14,24 @@ module buffer(data_count, scanning, flush, transfer, clk ,rst);
 	always@(posedge tBase[20]) begin
 		case(semaphore)
 			hold: data_count <= data_count;
-			increase: if(data_count < 7'd100)
-						data_count <= data_count + 1'b1;
-			decrease: if(data_count > 7'd0)
-						data_count <= data_count - 1'b2;
-			dump: data_count <= 7'd0;
-			default: data_count <= data_count;
+			increase: 	if(data_count < 8'd100)
+							data_count <= data_count + 8'd1;
+			decrease: 	if (data_count = 8'd1) 
+							data_count <= 8'd0;
+						else if(data_count > 8'd0)
+							data_count <= data_count - 8'd2;
+			dump: 	
+			default: 	data_count <= data_count;
 		endcase
 	end
 	
 	always@(posedge clk or negedge rst) begin
 		if(~rst)
 			semaphore <= dump;
+			data_count <= 8'd0;
 		else if (flush)
 			semaphore <= dump;
+			data_count <= 8'd0;
 		else if (scanning)
 			semaphore <= increase;
 		else if (transfer)
